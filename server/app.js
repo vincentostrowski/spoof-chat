d85e408e4544d8bb5c00c7d04a1ca6515c9a7a55
vincentostrovski <92182422+vincentostrovski@users.noreply.github.com>
2024-01-06 20:14:40 -0500

    Add authentication middleware and update dependencies

--- server/app.js
+++ server/app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
+require("express-async-errors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+const middleware = require("./utils/middleware");
 const cors = require("cors");
+const usersRouter = require("./routes/users");
+const conversationsRouter = require("./routes/conversations");
+const messagesRouter = require("./routes/messages");
+const loginRouter = require("./routes/login");
 
 mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
+
+app.use("/api/login", loginRouter);
+app.use("/api/users", usersRouter);
+app.use(middleware.authenticateToken);
+app.use("/api/conversations", conversationsRouter);
+app.use("/api/messages", messagesRouter);
+app.use(middleware.unkownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,10 +1,13 @@
-//error handling middleware
+const jwt = require("jsonwebtoken");
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).send({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: error.message });
   }
   next(error);
 };
@@ -13,7 +16,31 @@ const unkownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const authenticateToken = async (req, res, next) => {
+  const authorization = req.get("authorization");
+  let token = null;
+
+  if (authorization && authorization.startsWith("Bearer ")) {
+    token = authorization.replace("Bearer ", "");
+  }
+
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+  if (!user) {
+    return res.status(401).json({ error: "token invalid" });
+  }
+
+  req.user = user;
+  next();
+};
+
 module.exports = {
   errorHandler,
   unkownEndpoint,
+  authenticateToken,
 };
